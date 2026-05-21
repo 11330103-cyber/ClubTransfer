340aa70ccb76b537eda24fbcfd5050e273d628a3
Kinjin <11330103@dcsh.tp.edu.tw>
2026-05-22 01:15:30 +0800

    權限與申請

--- transfer/urls.py
+++ transfer/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('apply/', views.apply_transfer, name='apply_transfer'),
     path('progress/', views.progress, name='progress'),
-    #path('approve/<int:request_id>/', views.approve_request, name='approve_request'),
-    path('approves/',views.approve_request,name='approve_list'),
+    path('approve/', views.pending_approvals, name='pending_approvals'),
 ]
