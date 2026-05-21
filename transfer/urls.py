1b3fb8e6a98ef970666e1f3f6a0d1809728e28b6
Kinjin <11330103@dcsh.tp.edu.tw>
2026-05-21 22:35:45 +0800

    測試

--- transfer/urls.py
+++ transfer/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('apply/', views.apply_transfer, name='apply_transfer'),
     path('progress/', views.progress, name='progress'),
-    path('approve/<int:request_id>/', views.approve_request, name='approve_request'),
-    #path('approves/',views.approve_list,name='approve_list'),
+    #path('approve/<int:request_id>/', views.approve_request, name='approve_request'),
+    path('approves/',views.approve_request,name='approve_list'),
 ]
